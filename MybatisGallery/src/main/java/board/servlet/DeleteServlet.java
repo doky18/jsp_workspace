e91575685462b7b0ff1cf07be084cbb9421871a0
doky18 <114596747+doky18@users.noreply.github.com>
2023-01-11 23:14:15 +0900

    Update DeleteServlet.java

--- MybatisGallery/src/main/java/board/servlet/DeleteServlet.java
+++ MybatisGallery/src/main/java/board/servlet/DeleteServlet.java
@@ -1,5 +1,21 @@
 package board.servlet;
 
 public class DeleteServlet {
+  
+  if(file.delete()) { //파일이 삭제되었다면...
+            int result=boardDAO.delete(Integer.parseInt(board_idx));
+
+            out.print("<script>");
+            if(result>0) {
+                out.print("alert('삭제완료');");
+                out.print("location.href='/board/list.jsp';");
+            }else {
+                out.print("alert('삭제실패');");
+                out.print("history.back();");
+            }
+            out.print("</script>");
+        }
+
+    }
 
 }
